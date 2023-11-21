dee6ca0732a3306907325b7195737ae1512684e7
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2023-11-21 16:01:08 +0300

    Add project files.

---
+++ SemaphorePlus/Program.cs
@@ -0,0 +1,20 @@
+﻿using SemaphorePlus;
+
+var semaphoreSlimDelayed = new SemaphoreSlimDelayed(360, 1 * 60 * 1000);
+semaphoreSlimDelayed.Init();
+
+foreach (var item in Enumerable.Range(0, 1000))
+{
+    await Task.Delay(Random.Shared.Next(10, 20));
+    Task.Run(async () =>
+    {
+        Console.WriteLine("I'm Started " + item.ToString());
+        await semaphoreSlimDelayed.RegisterAsync();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("I'm Have Been Done " + item.ToString());
+        Console.ForegroundColor = ConsoleColor.White;
+
+    });
+}
+
+Console.ReadKey();
---
+++ SemaphorePlus/SemaphoreSlimDelayed.cs
@@ -0,0 +1,52 @@
+﻿namespace SemaphorePlus;
+public class SemaphoreSlimDelayed : SemaphoreSlim
+{
+    private readonly SemaphoreSlim _semaphore = new(1, 1);
+
+    private readonly int maxCount;
+    private readonly int milliseconds;
+    private int connectedCount = 0;
+    public SemaphoreSlimDelayed(int maxCount, int milliseconds) : base(1, maxCount)
+    {
+        this.maxCount = maxCount;
+        this.milliseconds = milliseconds;
+    }
+
+    public int ConnectedCount => connectedCount;
+
+    public async Task RegisterAsync()
+    {
+        _semaphore.Wait();
+        connectedCount++;
+        //Console.ForegroundColor = ConsoleColor.Green;
+        //Console.WriteLine("{" + connectedCount + "} It's Connected Count");
+        //Console.ForegroundColor = ConsoleColor.White;
+        _semaphore.Release();
+        if (connectedCount >= maxCount)
+        {
+            await WaitAsync();
+        }
+    }
+    public void Init()
+    {
+        WaitForInit();
+    }
+    private void WaitForInit()
+    {
+        Task.Run(async () =>
+        {
+            await Task.Delay(milliseconds);
+            if (connectedCount > maxCount)
+            {
+                for (int i = 0; i < maxCount; i++)
+                {
+                    Release();
+                }
+                connectedCount -= maxCount;
+            }
+            else
+                connectedCount = 0;
+            WaitForInit();
+        });
+    }
+}
